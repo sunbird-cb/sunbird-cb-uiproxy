1a8ce3f227babd6805909925ace7f5cee61f091b
SuvajitTarento <suvajit.chakraborty@tarento.com>
2021-07-14 16:58:02 +0530

    Task54765- Changes for redirect

--- src/protectedApi_v8/resource.ts
+++ src/protectedApi_v8/resource.ts
@@ -1,6 +1,27 @@
 import { Router } from 'express'
 export const userAuthKeyCloakApi = Router()
 userAuthKeyCloakApi.get('/', async (req, res) => {
-    res.redirect(`https://${req.get('host')}/`) // Prod setup
-    // res.redirect('http://localhost:3000/') // Test setup
+    const host = req.get('host')
+    let redirectUrl = ''
+    switch (host) {
+        case 'igot-dev.in':
+            redirectUrl = 'https://' + host + '/page/home'
+            break
+        case 'mdo.igot-dev.in':
+            redirectUrl = 'https://' + host + '/app/home/welcome'
+            break
+        case 'spv.igot-dev.in':
+            redirectUrl = 'https://' + host + '/app/home/directory'
+            break
+        case 'cbc.igot-dev.in':
+            redirectUrl = 'https://' + host + '/app/home/welcome'
+            break
+        case 'cbp.igot-dev.in':
+            redirectUrl = 'https://' + host + '/author/cbp/me'
+            break
+        default:
+            redirectUrl = 'http://localhost:3000/app/home/directory'  // Test setup
+            break
+    }
+    res.redirect(redirectUrl)
 })

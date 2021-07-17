0d1d3d26bd0fe9fc57270cf73f29ddbfc17c768e
SuvajitTarento <suvajit.chakraborty@tarento.com>
2021-07-17 08:34:14 +0530

    Task54765-Changes for redirect

--- src/protectedApi_v8/resource.ts
+++ src/protectedApi_v8/resource.ts
@@ -2,34 +2,20 @@ import { Router } from 'express'
 const _                 = require('lodash')
 export const userAuthKeyCloakApi = Router()
 userAuthKeyCloakApi.get('/', async (req, res) => {
-    let host = req.get('host')
+    const host = req.get('host')
     let queryParam = ''
+    let isLocal = 0
     if (!_.isEmpty(req.query)) {
         queryParam = req.query.q
         if (queryParam.includes('localhost')) {
-            host = queryParam
+            isLocal = 1
         }
     }
     let redirectUrl = ''
-    switch (host) {
-        case 'igot-dev.in':
-            redirectUrl = 'https://' + host + '/page/home'
-            break
-        case 'mdo.igot-dev.in':
-            redirectUrl = 'https://' + host + '/app/home/welcome'
-            break
-        case 'spv.igot-dev.in':
-            redirectUrl = 'https://' + host + '/app/home/directory'
-            break
-        case 'cbc.igot-dev.in':
-            redirectUrl = 'https://' + host + '/app/home/welcome'
-            break
-        case 'cbp.igot-dev.in':
-            redirectUrl = 'https://' + host + '/author/cbp/me'
-            break
-        default:
-            redirectUrl = queryParam  // local setup
-            break
+    if (isLocal) {
+        redirectUrl = queryParam
+    } else {
+        redirectUrl = `https://${host}${queryParam}` //   'https://' + host + '/page/home'
     }
     res.redirect(redirectUrl)
 })

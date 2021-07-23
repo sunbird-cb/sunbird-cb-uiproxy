6049d3af7152a37709e99853974433019381d9d9
Karthikeyan Rajendran <70887864+karthik-tarento@users.noreply.github.com>
2021-07-23 11:32:30 +0530

    Adding user related API mappings in Proxy

--- src/proxies_v8/proxies_v8.ts
+++ src/proxies_v8/proxies_v8.ts
@@ -215,6 +215,10 @@ proxiesV8.use('/org/*',
   proxyCreatorSunbird(express.Router(), `${CONSTANTS.KONG_API_BASE}`)
 )
 
+proxiesV8.use('/user/*',
+  proxyCreatorSunbird(express.Router(), `${CONSTANTS.KONG_API_BASE}`)
+)
+
 // proxiesV8.use('/api/framework/*',
 //   // tslint:disable-next-line: max-line-length
 //   proxyCreatorQML(express.Router(), `${CONSTANTS.KONG_API_BASE}`, '/api/')

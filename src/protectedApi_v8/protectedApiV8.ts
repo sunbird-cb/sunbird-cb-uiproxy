7828448568ba5f9b716ec8f912b2d9dcd981e344
SuvajitTarento <suvajit.chakraborty@tarento.com>
2021-08-04 19:58:41 +0530

    Lint fix an removed file - [SC]

--- src/protectedApi_v8/protectedApiV8.ts
+++ src/protectedApi_v8/protectedApiV8.ts
@@ -29,15 +29,15 @@ protectedApiV8.get('/', (_req, res) => {
 protectedApiV8.use('/catalog', catalogApi) // Valid
 protectedApiV8.use('/cohorts', cohortsApi) // Valid
 protectedApiV8.use('/profanity', contentValidationApi) // Valid
-protectedApiV8.use('/discussionHub', discussionHubApi) //Valid
+protectedApiV8.use('/discussionHub', discussionHubApi) // Valid
 protectedApiV8.use('/scrom', scromApi) // Valid
 protectedApiV8.use('/social', socialApi) // Valid
 protectedApiV8.use('/user', user) // Valid
 protectedApiV8.use('/connections', connectionsApi) // Valid
 protectedApiV8.use('/portal', portalApi) // Valid
 protectedApiV8.use('/scroing', scoringApi) // Valid
-protectedApiV8.use('/workflowhandler', workflowHandlerApi) //Valid
+protectedApiV8.use('/workflowhandler', workflowHandlerApi) // Valid
 protectedApiV8.use('/roleactivity', roleActivityApi) // Valid
 protectedApiV8.use('/resource', userAuthKeyCloakApi) // Valid
-protectedApiV8.use('/workallocation', workAllocationApi) //Valid
+protectedApiV8.use('/workallocation', workAllocationApi) // Valid
 protectedApiV8.use('/frac', fracApi) // Valid

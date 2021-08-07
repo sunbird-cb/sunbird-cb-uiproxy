3a79828d9428296c96168a2b45e6975b5485d84e
SuvajitTarento <suvajit.chakraborty@tarento.com>
2021-08-07 10:44:29 +0530

    Added new api to whitelist

--- src/utils/whitelistApis.ts
+++ src/utils/whitelistApis.ts
@@ -1062,6 +1062,13 @@ export const API_LIST = {
                 ROLE.PUBLIC,
             ],
         },
+        '/protected/v8/user/evaluate/assessment/submit/v2': {
+            checksNeeded: [CHECK.ROLE],
+            // tslint:disable-next-line: object-literal-sort-keys
+            ROLE_CHECK: [
+                ROLE.PUBLIC,
+            ],
+        },
         '/reset': {
             checksNeeded: [],
             // tslint:disable-next-line: object-literal-sort-keys
@@ -1218,6 +1225,7 @@ export const API_LIST = {
         '/protected/v8/user/profileRegistry/getUserRegistryById',
         '/protected/v8/user/profileRegistry/getUserRegistryById/:id',
         '/protected/v8/workallocation/copy/workOrder',
+        '/protected/v8/user/evaluate/assessment/submit/v2',
         '/reset',
     ],
 }

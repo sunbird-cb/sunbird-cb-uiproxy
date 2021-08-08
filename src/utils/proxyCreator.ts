43f9a53cf2757933e7d1d8d66518fde4e6acc975
pilliarunkumar <arunkumar.pilli@tarento.com>
2021-08-08 19:21:50 +0530

    removed uid from proxy

--- src/utils/proxyCreator.ts
+++ src/utils/proxyCreator.ts
@@ -21,7 +21,6 @@ proxy.on('proxyReq', (proxyReq: any, req: any, _res: any, _options: any) => {
   // condition has been added to set the session in nodebb req header
   if (req.originalUrl.includes('/discussion') && !req.originalUrl.includes('/discussion/user/v1/create')) {
     proxyReq.setHeader('nodebb_authorization_token', 'Bearer ' + req.session.nodebb_authorization_token)
-    proxyReq.query._uid = req.session._uid
   }
 
   if (req.body) {

ad1d07922e00287054d18711ddb5f43ef1da231c
Amit Sengar <aAmitSengar@users.noreply.github.com>
2021-11-12 21:10:22 +0530

    Update whitelistApis.ts

--- src/utils/whitelistApis.ts
+++ src/utils/whitelistApis.ts
@@ -1059,6 +1059,7 @@ export const API_LIST = {
             // tslint:disable-next-line: object-literal-sort-keys
             ROLE_CHECK: [
                 ROLE.CONTENT_CREATOR,
+                ROLE.CONTENT_REVIEWER,
                 ROLE.EDITOR,
             ],
         },

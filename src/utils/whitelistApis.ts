22a3480ca64d1dea118b6684f20d0bb816ab89aa
karthik-tarento <karthikeyan.rajendran@tarento.com>
2022-05-17 14:22:54 +0530

    Added role check for Reset API

--- src/utils/whitelistApis.ts
+++ src/utils/whitelistApis.ts
@@ -1304,7 +1304,7 @@ export const API_LIST = {
             ],
         },
         '/reset': {
-            checksNeeded: [],
+            checksNeeded: [CHECK.ROLE],
             // tslint:disable-next-line: object-literal-sort-keys
             ROLE_CHECK: [
                 ROLE.PUBLIC,

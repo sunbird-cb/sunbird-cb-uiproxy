7f3f513448de8dc4cb44c5a5c53770ea7b8f6a8b
karthik-tarento <karthikeyan.rajendran@tarento.com>
2022-07-01 09:18:39 +0530

    updated redirect url

--- src/publicApi_v8/googleauth.ts
+++ src/publicApi_v8/googleauth.ts
@@ -7,7 +7,7 @@ export const googleAuth = express.Router()
 
 googleAuth.get('/auth', async (req, res) => {
     logInfo('Received host ? ' + req.hostname)
-    let oAuthParams = 'client_id=' + CONSTANTS.GOOGLE_CLIENT_ID + '&redirect_uri=https://igot-dev.in/google/callback&prompt=consent'
+    let oAuthParams = 'client_id=' + CONSTANTS.GOOGLE_CLIENT_ID + '&redirect_uri=https://igot-dev.in/apis/public/v8/google/callback&prompt=consent'
     oAuthParams = oAuthParams + '&response_type=code&scope=https://www.googleapis.com/auth/userinfo.email'
     const googleUrl = 'https://accounts.google.com/o/oauth2/v2/auth?' + oAuthParams
     logInfo('google Url -> ' + googleUrl)

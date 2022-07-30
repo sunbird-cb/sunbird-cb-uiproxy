ab526285ecdb8dcc50e563260e4f749f45464781
karthik-tarento <karthikeyan.rajendran@tarento.com>
2022-07-30 07:12:15 +0530

    Removed google route which is added for testing

--- src/publicApi_v8/publicApiV8.ts
+++ src/publicApi_v8/publicApiV8.ts
@@ -1,8 +1,6 @@
 import express from 'express'
 import { CONSTANTS } from '../utils/env'
 import { proxyCreatorRoute } from '../utils/proxyCreator'
-import { googleAuth } from './googleauth'
-import { parichayAuth } from './parichayAuth'
 import { workallocationPublic } from './workallocationPublic'
 
 export const publicApiV8 = express.Router()
@@ -19,7 +17,3 @@ publicApiV8.use('/assets',
 publicApiV8.use('/workallocation', workallocationPublic)
 
 publicApiV8.use('/org/v1/list', proxyCreatorRoute(express.Router(), CONSTANTS.KONG_API_BASE + '/org/v1/list'))
-
-publicApiV8.use('/google', googleAuth)
-
-publicApiV8.use('/parichay', parichayAuth)

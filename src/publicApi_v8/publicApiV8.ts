12f097540e78105c2e01a0ca9e7abba1e8f3c121
karthik-tarento <karthikeyan.rajendran@tarento.com>
2022-08-01 15:28:07 +0530

    Added google auth for testing

--- src/publicApi_v8/publicApiV8.ts
+++ src/publicApi_v8/publicApiV8.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import { CONSTANTS } from '../utils/env'
 import { proxyCreatorRoute } from '../utils/proxyCreator'
+import { googleAuth } from './googleauth'
 import { parichayAuth } from './parichayAuth'
 import { workallocationPublic } from './workallocationPublic'
 
@@ -20,3 +21,5 @@ publicApiV8.use('/workallocation', workallocationPublic)
 publicApiV8.use('/org/v1/list', proxyCreatorRoute(express.Router(), CONSTANTS.KONG_API_BASE + '/org/v1/list'))
 
 publicApiV8.use('/parichay', parichayAuth)
+
+publicApiV8.use('/google', googleAuth)

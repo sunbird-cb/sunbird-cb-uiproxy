4fca8d4db0e5dd35e05b401353e813d070b0b2f4
Darshan Nagesh <darshan.nagesh@tarento.com>
2022-08-16 19:40:28 +0530

    Update whitelistApis.ts
    Get All Applications API to be whilelisted

--- src/utils/whitelistApis.ts
+++ src/utils/whitelistApis.ts
@@ -346,6 +346,13 @@ export const API_LIST = {
                 ROLE.PUBLIC,
             ],
         },
+        '/proxies/v8/forms/getAllApplications': {
+            checksNeeded: [CHECK.ROLE],
+            // tslint:disable-next-line: object-literal-sort-keys
+            ROLE_CHECK: [
+                ROLE.PUBLIC,
+            ],
+        },
         '/proxies/v8/upload/action/content/v3/upload/:do_id': {
             checksNeeded: [CHECK.ROLE],
             // tslint:disable-next-line: object-literal-sort-keys
@@ -1944,6 +1951,7 @@ export const API_LIST = {
             '/proxies/v8/forms/getFormById',
             '/proxies/v8/forms/getAllForms',
             '/proxies/v8/forms/v1/saveFormSubmit',
+            '/proxies/v8/forms/getAllApplications', 
             '/protected/v8/user/profileDetails/test',
             '/protected/v8/resource/',
             '/protected/v8/user/details',

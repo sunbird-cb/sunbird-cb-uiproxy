f176ab2d4961c4d03d495e4fc92ecbef3f7697e1
Amit Sengar <aAmitSengar@users.noreply.github.com>
2022-09-06 11:08:56 +0530

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -73,9 +73,9 @@ export class Server {
       }
       next()
     })
-    this.app.use((_req: express.Request, res: express.Response, next: express.NextFunction) => {
+    this.app.use((_req: express.Request, _res: express.Response, next: express.NextFunction) => {
       // tslint:disable
-      res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate')
+      // res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate')
       // res.header('Expires', '-1')
       // res.header('Pragma', 'no-cache')
       // tslint:enable

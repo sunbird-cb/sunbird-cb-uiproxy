4c565b4ea763c57f74f6142b404d39298ff1830e
juhi agarwal <juhiagl8@gmail.com>
2023-02-21 18:25:05 +0530

    Changes for the Content Flagging APIs (#134)
    * Update whitelistApis.ts (#132)

--- src/utils/whitelistApis.ts
+++ src/utils/whitelistApis.ts
@@ -2006,6 +2006,20 @@ export const API_LIST = {
                 ROLE.MDO_ADMIN,
             ],
         },
+      '/proxies/v8/user/offensive/data/flag': {
+            checksNeeded: [CHECK.ROLE],
+            // tslint:disable-next-line: object-literal-sort-keys
+            ROLE_CHECK: [
+                ROLE.PUBLIC,
+            ],
+        },
+      '/proxies/v8/user/offensive/data/flag/getFlaggedData': {
+            checksNeeded: [CHECK.ROLE],
+            // tslint:disable-next-line: object-literal-sort-keys
+            ROLE_CHECK: [
+                ROLE.PUBLIC,
+            ],
+        },
     },
     URL_PATTERN:
         [
@@ -2273,5 +2287,7 @@ export const API_LIST = {
             '/proxies/v8/mdo/content/v3/publish/:do_id',
             '/proxies/v8/mdo/content/v3/batch/addUser',
             '/proxies/v8/mdo/content/v3/batch/removeUser',
+            '/proxies/v8/user/offensive/data/flag',
+            '/proxies/v8/user/offensive/data/flag/getFlaggedData',
            ],
 }

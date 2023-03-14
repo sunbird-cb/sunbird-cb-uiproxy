cb86b12f935b02ea39349168ed3c7d26ee5bdb7a
karthik-tarento <karthikeyan.rajendran@tarento.com>
2023-03-14 11:31:05 +0530

    Using secure and sameSite attributes in Cookie

--- src/configs/session.config.ts
+++ src/configs/session.config.ts
@@ -39,6 +39,8 @@ export function getSessionConfig(
     sessionConfig = {
       cookie: {
         maxAge: CONSTANTS.KEYCLOAK_SESSION_TTL,
+        sameSite: 'None',
+        secure: true
       },
       resave: false,
       saveUninitialized: false,

c639cfd2b925e092b96660a951f5ce05ba42cde4
karthik-tarento <karthikeyan.rajendran@tarento.com>
2023-03-29 13:34:16 +0530

    Removed secure flag

--- src/configs/session.config.ts
+++ src/configs/session.config.ts
@@ -39,8 +39,7 @@ export function getSessionConfig(
     sessionConfig = {
       cookie: {
         maxAge: CONSTANTS.KEYCLOAK_SESSION_TTL,
-        sameSite: 'None',
-        secure: true
+        sameSite: 'Lax',
       },
       resave: false,
       saveUninitialized: false,

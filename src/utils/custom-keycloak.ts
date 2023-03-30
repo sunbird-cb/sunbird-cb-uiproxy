efb3c0a8daf69e3b45b89fd333b31f45b3b1dddc
karthik-tarento <karthikeyan.rajendran@tarento.com>
2023-03-30 17:58:53 +0530

    Setting cookie after keycloak authentication

--- src/utils/custom-keycloak.ts
+++ src/utils/custom-keycloak.ts
@@ -55,10 +55,11 @@ export class CustomKeycloak {
   }
 
   // tslint:disable-next-line: no-any
-  authenticated = (reqObj: any, next: any) => {
+  authenticated = (reqObj: any, resObj: any, next: any) => {
     logInfo('Cookie from request object, As it is: ', JSON.stringify(reqObj.session.cookie))
     reqObj.session.cookie.secure = true
     logInfo('Cookie from request object, After updating: ', JSON.stringify(reqObj.session.cookie))
+    resObj.headers['set-cookie'] = reqObj.session.cookie
     logInfo('Step 3: authenticated function', '------', new Date().toString())
     try {
       const userId = reqObj.kauth.grant.access_token.content.sub.split(':')

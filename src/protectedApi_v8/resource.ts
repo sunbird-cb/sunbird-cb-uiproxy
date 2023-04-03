96ba25ff76677cdb1b8d3cced348a667ab532463
karthik-tarento <karthikeyan.rajendran@tarento.com>
2023-04-03 15:46:20 +0530

    Added secure and sameSite flags in cookie

--- src/protectedApi_v8/resource.ts
+++ src/protectedApi_v8/resource.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import { CONSTANTS } from '../utils/env'
 import { logInfo } from '../utils/logger'
 const _                 = require('lodash')
 export const userAuthKeyCloakApi = Router()
@@ -6,7 +7,16 @@ userAuthKeyCloakApi.get('/', (req, res) => {
     const host = req.get('host')
     let queryParam = ''
     let isLocal = 0
-    logInfo('Received query param: ' + req.query)
+    logInfo('Received query param: ' + JSON.stringify(req.query))
+    if (req.session && req.session.authenticated ) {
+        logInfo('User is authenticated.. Updating Cookie with Secure and SameSite flags')
+        res.cookie('connect.sid', req.cookies['connect.sid'], {
+            httpOnly: true,
+            maxAge: CONSTANTS.KEYCLOAK_SESSION_TTL,
+            sameSite: 'None',
+            secure: true,
+        })
+    }
     if (!_.isEmpty(req.query)) {
         queryParam = req.query.q
         if (queryParam && queryParam.includes('localhost')) {

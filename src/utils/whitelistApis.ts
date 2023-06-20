7e1e0f0ac6bd9524a88cd306711749c85df485cd
karthik-tarento <karthikeyan.rajendran@tarento.com>
2023-06-20 19:01:57 +0530

    Added APIs for blended program

--- src/utils/whitelistApis.ts
+++ src/utils/whitelistApis.ts
@@ -2089,6 +2089,28 @@ export const API_LIST = {
                ROLE.MDO_ADMIN,
            ],
        },
+       '/proxies/v8/workflow/blendedprogram/enrol': {
+           checksNeeded: [CHECK.ROLE],
+           // tslint:disable-next-line: object-literal-sort-keys
+           ROLE_CHECK: [
+               ROLE.PUBLIC,
+           ],
+       },
+       '/proxies/v8/workflow/blendedprogram/search': {
+            checksNeeded: [CHECK.ROLE],
+            // tslint:disable-next-line: object-literal-sort-keys
+            ROLE_CHECK: [
+                ROLE.PROGRAM_COORDINATOR,
+                ROLE.MDO_ADMIN,
+            ],
+        },
+        '/proxies/v8/workflow/blendedprogram/user/search': {
+            checksNeeded: [CHECK.ROLE],
+            // tslint:disable-next-line: object-literal-sort-keys
+            ROLE_CHECK: [
+                ROLE.PUBLIC,
+            ],
+        },
     },
     URL_PATTERN:
         [
@@ -2367,5 +2389,8 @@ export const API_LIST = {
             '/proxies/v8/workflow/position/search',
             '/proxies/v8/workflow/org/search',
             '/proxies/v8/workflow/domain/search',
+            '/proxies/v8/workflow/blendedprogram/enrol',
+            '/proxies/v8/workflow/blendedprogram/search',
+            '/proxies/v8/workflow/blendedprogram/user/search',
            ],
 }

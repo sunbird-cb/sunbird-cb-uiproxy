5f138bc307582300a864103ade9c279363618661
karthik-tarento <karthikeyan.rajendran@tarento.com>
2023-09-30 09:51:01 +0530

    Added SPV_PUBLISHER role

--- src/utils/whitelistApis.ts
+++ src/utils/whitelistApis.ts
@@ -209,6 +209,8 @@ export const API_LIST = {
             ROLE_CHECK: [
                 ROLE.CONTENT_REVIEWER,
                 ROLE.CONTENT_PUBLISHER,
+                ROLE.SPV_PUBLISHER,
+                ROLE.SPV_PUBLISHER,
             ],
         },
         '/proxies/v8/discussion/forum/v2/read': {
@@ -259,6 +261,7 @@ export const API_LIST = {
             ROLE_CHECK: [
                 ROLE.CONTENT_PUBLISHER,
                 ROLE.CONTENT_REVIEWER,
+                ROLE.SPV_PUBLISHER,
             ],
         },
         '/proxies/v8/action/content/v3/hierarchy/update': {
@@ -269,6 +272,7 @@ export const API_LIST = {
                 ROLE.EDITOR,
                 ROLE.CONTENT_PUBLISHER,
                 ROLE.CONTENT_REVIEWER,
+                ROLE.SPV_PUBLISHER,
             ],
         },
         '/proxies/v8/action/content/v3/create': {
@@ -409,6 +413,7 @@ export const API_LIST = {
             ROLE_CHECK: [
                 ROLE.CONTENT_REVIEWER,
                 ROLE.CONTENT_PUBLISHER,
+                ROLE.SPV_PUBLISHER,
             ],
         },
         '/proxies/v8/action/content/v3/review/:do_id': {
@@ -418,6 +423,7 @@ export const API_LIST = {
                 ROLE.CONTENT_CREATOR,
                 ROLE.CONTENT_REVIEWER,
                 ROLE.CONTENT_PUBLISHER,
+                ROLE.SPV_PUBLISHER,
             ],
         },
         '/proxies/v8/action/content/v3/publish/:do_id': {
@@ -426,6 +432,7 @@ export const API_LIST = {
             ROLE_CHECK: [
                 ROLE.CONTENT_PUBLISHER,
                 ROLE.MDO_ADMIN,
+                ROLE.SPV_PUBLISHER,
             ],
         },
         '/proxies/v8/action/content/v3/update/:do_id': {
@@ -438,6 +445,7 @@ export const API_LIST = {
                 ROLE.CONTENT_PUBLISHER,
                 ROLE.MDO_ADMIN,
                 ROLE.MDO_LEADER,
+                ROLE.SPV_PUBLISHER,
             ],
         },
         '/proxies/v8/data/v1/system/settings/get/orgTypeList': {
@@ -587,6 +595,7 @@ export const API_LIST = {
             // tslint:disable-next-line: object-literal-sort-keys
             ROLE_CHECK: [
                 ROLE.CONTENT_PUBLISHER,
+                ROLE.SPV_PUBLISHER,
             ],
         },
         '/proxies/v8/learnervm/private/content/v3/review/:do_id': {
@@ -1150,6 +1159,7 @@ export const API_LIST = {
             // tslint:disable-next-line: object-literal-sort-keys
             ROLE_CHECK: [
                 ROLE.PUBLIC,
+                ROLE.SPV_PUBLISHER,
             ],
         },
         '/protected/v8/portal/cbc/department': {
@@ -1181,6 +1191,7 @@ export const API_LIST = {
                 ROLE.EDITOR,
                 ROLE.CONTENT_PUBLISHER,
                 ROLE.CONTENT_REVIEWER,
+                ROLE.SPV_PUBLISHER,
             ],
         },
         // '/protected/v8/connections/connections/recommended': {
@@ -1254,6 +1265,7 @@ export const API_LIST = {
                 ROLE.EDITOR,
                 ROLE.CONTENT_PUBLISHER,
                 ROLE.CONTENT_REVIEWER,
+                ROLE.SPV_PUBLISHER,
             ],
         },
         '/protected/v8/profanity/getPdfProfanityForContent/:contentId': {
@@ -1264,6 +1276,7 @@ export const API_LIST = {
                 ROLE.CONTENT_REVIEWER,
                 ROLE.CONTENT_PUBLISHER,
                 ROLE.EDITOR,
+                ROLE.SPV_PUBLISHER,
             ],
         },
         '/protected/v8/catalog': {
@@ -1283,6 +1296,7 @@ export const API_LIST = {
                 ROLE.CONTENT_REVIEWER,
                 ROLE.EDITOR,
                 ROLE.CONTENT_PUBLISHER,
+                ROLE.SPV_PUBLISHER,
             ],
         },
         '/protected/v8/portal/mdo/deptAction/userrole': {
@@ -1841,6 +1855,7 @@ export const API_LIST = {
                 ROLE.CONTENT_REVIEWER,
                 ROLE.MDO_ADMIN,
                 ROLE.MDO_LEADER,
+                ROLE.SPV_PUBLISHER,
             ],
         },
         '/proxies/v8/questionset/v1/create': {
@@ -1858,6 +1873,7 @@ export const API_LIST = {
                 ROLE.CONTENT_REVIEWER,
                 ROLE.MDO_ADMIN,
                 ROLE.MDO_LEADER,
+                ROLE.SPV_PUBLISHER,
             ],
             checksNeeded: [CHECK.ROLE],
         },
@@ -1868,6 +1884,7 @@ export const API_LIST = {
                 ROLE.CONTENT_REVIEWER,
                 ROLE.MDO_ADMIN,
                 ROLE.MDO_LEADER,
+                ROLE.SPV_PUBLISHER,
             ],
             checksNeeded: [CHECK.ROLE],
         },
@@ -1884,6 +1901,7 @@ export const API_LIST = {
                 ROLE.CONTENT_PUBLISHER,
                 ROLE.MDO_ADMIN,
                 ROLE.MDO_LEADER,
+                ROLE.SPV_PUBLISHER,
             ],
             checksNeeded: [CHECK.ROLE],
         },
@@ -1900,6 +1918,7 @@ export const API_LIST = {
                 ROLE.CONTENT_CREATOR,
                 ROLE.CONTENT_PUBLISHER,
                 ROLE.CONTENT_REVIEWER,
+                ROLE.SPV_PUBLISHER,
             ],
             checksNeeded: [CHECK.ROLE],
         },
@@ -1907,6 +1926,7 @@ export const API_LIST = {
             ROLE_CHECK: [
                 ROLE.CONTENT_PUBLISHER,
                 ROLE.CONTENT_REVIEWER,
+                ROLE.SPV_PUBLISHER,
             ],
             checksNeeded: [CHECK.ROLE],
         },
@@ -1917,6 +1937,7 @@ export const API_LIST = {
                 ROLE.CONTENT_REVIEWER,
                 ROLE.MDO_ADMIN,
                 ROLE.MDO_LEADER,
+                ROLE.SPV_PUBLISHER,
             ],
             checksNeeded: [CHECK.ROLE],
         },

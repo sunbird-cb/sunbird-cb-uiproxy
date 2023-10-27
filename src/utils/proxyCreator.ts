7770d9f3eec4e641470c4c5f9d55ba9ad413a837
SaipradeepR <53404427+SaipradeepR@users.noreply.github.com>
2023-10-27 12:52:31 +0530

    Update proxyCreator.ts
    Adding query params only if not already exist

--- src/utils/proxyCreator.ts
+++ src/utils/proxyCreator.ts
@@ -329,10 +329,12 @@ export function proxyAssessmentRead(route: Router, targetUrl: string, _timeout =
 
 export function proxyQuestionRead(route: Router, targetUrl: string, _timeout = 10000): Router {
   route.all('/*', (req, res) => {
+    if (!targetUrl.includes('?')) {
     // Split the URL into base URL and query parameters
     const [, queryParams] = req.originalUrl.split('?');
     // Construct the final target URL by appending query parameters
     targetUrl = targetUrl + (queryParams ? `?${queryParams}` : ''); 
+    }
     // tslint:disable-next-line: no-console 
     console.log('REQ_URL_UPDATED proxyAssessmentRead', targetUrl)
     proxy.web(req, res, {

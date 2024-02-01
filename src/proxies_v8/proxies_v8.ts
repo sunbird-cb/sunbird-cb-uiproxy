5c34474c676f11aa8b9bf253539fb3c965a1581c
dkttarento <138442957+dkttarento@users.noreply.github.com>
2024-02-01 18:32:32 +0530

    Update proxies_v8.ts
    for ehrms

--- src/proxies_v8/proxies_v8.ts
+++ src/proxies_v8/proxies_v8.ts
@@ -595,6 +595,11 @@ proxiesV8.use('/cbplan/*',
   proxyCreatorSunbird(express.Router(), `${CONSTANTS.KONG_API_BASE}`)
 )
 
+proxiesV8.use('/ehrms/*',
+  // tslint:disable-next-line: max-line-length
+  proxyCreatorSunbird(express.Router(), `${CONSTANTS.KONG_API_BASE}`)
+)
+
 function removePrefix(prefix: string, s: string) {
   return s.substr(prefix.length)
 }

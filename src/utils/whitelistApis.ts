f1696fa9cee27a95873bd62ce99e9c7efa8959a1
tarentomaheshvakkund <139739142+tarentomaheshvakkund@users.noreply.github.com>
2024-02-29 23:39:42 +0530

    KB-2925 | New API for the OTP generation for email/phone verification.
    1. Whitelisted the API

--- src/utils/whitelistApis.ts
+++ src/utils/whitelistApis.ts
@@ -2767,6 +2767,20 @@ export const API_LIST = {
                 ROLE.PUBLIC,
             ],
         },
+        '/proxies/v8/otp/v3/generate': {
+            checksNeeded: [CHECK.ROLE],
+            // tslint:disable-next-line: object-literal-sort-keys
+            ROLE_CHECK: [
+                ROLE.PUBLIC,
+            ],
+        },
+        '/proxies/v8/otp/v3/verify': {
+            checksNeeded: [CHECK.ROLE],
+            // tslint:disable-next-line: object-literal-sort-keys
+            ROLE_CHECK: [
+                ROLE.PUBLIC,
+              ],
+          },
     },
     URL_PATTERN:
         [
@@ -3120,5 +3134,7 @@ export const API_LIST = {
             '/proxies/v8/program/v2/admin/bulkEnroll',
             '/proxies/v8/operationalreports/admin/grantaccess',
             '/proxies/v8/openprogram/v1/enrol',
+            '/proxies/v8/otp/v3/generate',
+            '/proxies/v8/otp/v3/otp',
            ],
 }

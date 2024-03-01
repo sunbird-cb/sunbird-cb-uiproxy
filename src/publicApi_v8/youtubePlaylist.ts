3e00d3b838e0a3caacd4d12442da7cfbeb0ac675
sahilchaudhary <sahil.chaudhary@tarento.com>
2024-03-01 18:38:08 +0530

    API changes for youtube playslist

--- src/publicApi_v8/youtubePlaylist.ts
+++ src/publicApi_v8/youtubePlaylist.ts
@@ -6,12 +6,13 @@ import { logInfo } from '../utils/logger'
 
 export const youtubePlaylist = express.Router()
 
-youtubePlaylist.get('/landingpage', async (_, res) => {
+youtubePlaylist.get('/landingpage', async (req, res) => {
+    const playListId = req.query.playListId
     let playListUrl = CONSTANTS.YOUTUBE_PLAYLIST_URL
     playListUrl += '?key=' + CONSTANTS.YOUTUBE_PLAYLIST_API_KEY
-    playListUrl += '&playlistId=' + CONSTANTS.YOUTUBE_PLAYLIST_ID
+    playListUrl += '&playlistId=' + playListId
     playListUrl += '&part=snippet,id,contentDetails'
-    playListUrl += '&maxResults=' + CONSTANTS.YOUTUBE_PLAYLIST_MAX_RESULT
+    playListUrl += '&maxResults=' + Number(req.query.maxResults) || Number(CONSTANTS.YOUTUBE_PLAYLIST_MAX_RESULT)
     logInfo('Youtube playlist constructed url : ' + playListUrl)
     const playlistResponse = await axios({
         ...axiosRequestConfig,

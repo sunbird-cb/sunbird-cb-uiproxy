dc90b9fdba3811ebb24f9dc82a2ddfdf8959f2d8
tarentomaheshvakkund <139739142+tarentomaheshvakkund@users.noreply.github.com>
2024-03-05 09:38:32 +0530

    KB-2485 | BE | Dev | Create a new API for downloading a password-protected zip report
    1. Whitelist role added

--- src/utils/whitelistApis.ts
+++ src/utils/whitelistApis.ts
@@ -2772,6 +2772,7 @@ export const API_LIST = {
             // tslint:disable-next-line: object-literal-sort-keys
             ROLE_CHECK: [
                 ROLE.MDO_LEADER,
+                ROLE.MDO_ADMIN,
               ],
           },
     },

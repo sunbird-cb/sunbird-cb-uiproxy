be8e9850da445d95806ebbefa50745dd436fb11c
sahilchaudhary <sahil.chaudhary@tarento.com>
2024-05-31 12:08:40 +0530

    orgBookmark API addition

--- src/proxies_v8/proxies_v8.ts
+++ src/proxies_v8/proxies_v8.ts
@@ -929,6 +929,10 @@ proxiesV8.use('/careers/*',
   proxyCreatorSunbird(express.Router(), `${CONSTANTS.KONG_API_BASE}`)
 )
 
+proxiesV8.use('/orgBookmark/*',
+  proxyCreatorSunbird(express.Router(), `${CONSTANTS.KONG_API_BASE}`)
+)
+
 export interface IUserProfile {
   channel: string
   firstName: string

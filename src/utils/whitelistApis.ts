ffcd4d6674f068fc6967083480d1707c711bcfda
anil <anilkumar.kammalapalli@tarento.com>
2024-06-19 16:48:13 +0530

    added configurations for update bulk user

--- src/utils/whitelistApis.ts
+++ src/utils/whitelistApis.ts
@@ -3504,7 +3504,7 @@ export const API_LIST = {
                         ROLE.MDO_LEADER,
                     ],
         },
-         '/proxies/v8/workflow/admin/v2/transition/bulkupdate': {
+         '/proxies/v8/workflow/admin/v2/bulkupdate/transition': {
                     checksNeeded: [CHECK.ROLE],
                     // tslint:disable-next-line: object-literal-sort-keys
                     ROLE_CHECK: [
@@ -3961,6 +3961,6 @@ export const API_LIST = {
             '/proxies/v8/tenders/v4/create',
             '/proxies/v8/tenders/v4/update/:do_id',
             '/proxies/v8/user/v2/bulkupload',
-            '/proxies/v8/workflow/admin/v2/transition/bulkupdate'
+            '/proxies/v8/workflow/admin/v2/bulkupdate/transition'
            ],
 }

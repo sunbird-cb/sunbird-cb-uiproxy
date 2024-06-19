8544fea8bd13c99361a0cca7da7e245ce0745e91
ruksana2808 <111083125+ruksana2808@users.noreply.github.com>
2024-06-19 17:59:01 +0530

    Added ui-proxy for searchProgram API (#389)

--- src/utils/whitelistApis.ts
+++ src/utils/whitelistApis.ts
@@ -3512,7 +3512,13 @@ export const API_LIST = {
                         ROLE.MDO_LEADER,
                      ],
          },
-
+          '/proxies/v8/playLis/v1/search/program': {
+            checksNeeded: [CHECK.ROLE],
+            // tslint:disable-next-line: object-literal-sort-keys
+            ROLE_CHECK: [
+                ROLE.PUBLIC,
+            ],
+        },
     },
     URL_PATTERN:
         [
@@ -3962,5 +3968,6 @@ export const API_LIST = {
             '/proxies/v8/tenders/v4/update/:do_id',
             '/proxies/v8/user/v2/bulkupload',
             '/proxies/v8/workflow/admin/v2/bulkupdate/transition',
+            '/proxies/v8/playLis/v1/search/program',
            ],
 }

3c14ee992e68965fdca28169ff877c9d3bdeb1f5
Sahil-tarento <140611066+Sahil-tarento@users.noreply.github.com>
2024-06-25 16:51:24 +0530

    Adding competency API (#408)

--- src/proxies_v8/proxies_v8.ts
+++ src/proxies_v8/proxies_v8.ts
@@ -958,7 +958,7 @@ proxiesV8.use('/framework/*',
 )
 
 proxiesV8.use('/v1/search/competenciesByOrg',
-  proxyCreatorSunbird(express.Router(), `${CONSTANTS.KONG_API_BASE}/v1/search/competenciesByOrg`)
+  proxyCreatorSunbird(express.Router(), `${CONSTANTS.KONG_API_BASE}`)
 )
 
 export interface IUserProfile {

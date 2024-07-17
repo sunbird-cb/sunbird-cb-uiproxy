54bbe097f81ebc052c9ce59c1714588f6c969890
Sahil-tarento <140611066+Sahil-tarento@users.noreply.github.com>
2024-07-17 10:31:19 +0530

    Revert "ext enrollment read api updated (#425)"
    This reverts commit 2c93c6bd97755da922b0a98e441786894c288680.

--- src/utils/whitelistApis.ts
+++ src/utils/whitelistApis.ts
@@ -3642,14 +3642,14 @@ export const API_LIST = {
                       ROLE.PUBLIC,
                     ],
         },
-        '/proxies/v8/cios-enroll/v1/courselist/byuserid': {
+        '/proxies/v8/cios-enroll/v1/listbyuserid/:id': {
                     checksNeeded: [CHECK.ROLE],
                     // tslint:disable-next-line: object-literal-sort-keys
                     ROLE_CHECK: [
                       ROLE.PUBLIC,
                     ],
         },
-        '/proxies/v8/cios-enroll/v1/readby/useridcourseid/:courseid': {
+        '/proxies/v8/cios-enroll/v1/readby/useridcourseId/:userId/:courseId': {
                     checksNeeded: [CHECK.ROLE],
                     // tslint:disable-next-line: object-literal-sort-keys
                     ROLE_CHECK: [
@@ -4168,8 +4168,8 @@ export const API_LIST = {
             '/proxies/v8/designation/search',
             '/proxies/v8/framework/v1/copy/:id',
             '/proxies/v8/cios-enroll/v1/create',
-            '/proxies/v8/cios-enroll/v1/courselist/byuserid',
-            '/proxies/v8/cios-enroll/v1/readby/useridcourseid/:courseid',
+            '/proxies/v8/cios-enroll/v1/listbyuserid/:id',
+            '/proxies/v8/cios-enroll/v1/readby/useridcourseId/:userId/:courseId',
             '/proxies/v8/competencyArea/upload',
             '/proxies/v8/competencyArea/search',
             '/proxies/v8/competencyTheme/upload',

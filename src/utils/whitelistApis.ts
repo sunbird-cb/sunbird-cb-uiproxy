32a3ae62f7516b6caf2657568d4b326f0e77b138
tarentomaheshvakkund <139739142+tarentomaheshvakkund@users.noreply.github.com>
2024-07-18 12:26:56 +0530

    KB-5808 | Q5 | Enhancement | MDO Channel | A consolidated level learner leaderboard based on Karma Points
    1. Added the entry '/proxies/v8/halloffame/top/learners' to whitelist the user with the role as 'PUBLIC'.
    2. Added the entry to the proxies_v8 to redirect to the Kong if the url is matched.

--- src/utils/whitelistApis.ts
+++ src/utils/whitelistApis.ts
@@ -3973,6 +3973,13 @@ export const API_LIST = {
                     ROLE.PUBLIC,
                    ],
         },
+        '/proxies/v8/halloffame/top/learners': {
+                  checksNeeded: [CHECK.ROLE],
+                  // tslint:disable-next-line: object-literal-sort-keys
+                   ROLE_CHECK: [
+                    ROLE.PUBLIC,
+                   ],
+        },
     },
     URL_PATTERN:
         [
@@ -4488,5 +4495,6 @@ export const API_LIST = {
             '/proxies/v8/competencySubTheme/delete/:id',
             '/proxies/v8/competencySubTheme/update',
             '/proxies/v8/competencySubTheme/read/:id',
+            '/proxies/v8/halloffame/top/learners',
            ],
 }

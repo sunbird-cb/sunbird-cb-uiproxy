0c7a34c3d68ece176cad01681e113c4a2738910e
shankaragoudab <140387294+shankaragoudab@users.noreply.github.com>
2024-07-22 14:52:32 +0530

    added user v3 search api (#439)
    * added user v3 search api
    
    * added mdo roles

--- src/utils/whitelistApis.ts
+++ src/utils/whitelistApis.ts
@@ -4008,6 +4008,14 @@ export const API_LIST = {
                                   ROLE.MDO_ADMIN,
                                  ],
         },
+        '/proxies/v8/user/v3/search': {
+            checksNeeded: [CHECK.ROLE],
+            // tslint:disable-next-line: object-literal-sort-keys
+            ROLE_CHECK: [
+                ROLE.MDO_ADMIN,
+                ROLE.MDO_LEADER,
+            ],
+        },
     },
     URL_PATTERN:
         [
@@ -4528,5 +4536,6 @@ export const API_LIST = {
             '/proxies/v8/competencyTheme/create/term',
             '/proxies/v8/designation/create/term',
             '/proxies/v8/org/framework/read',
+            '/proxies/v8/user/v3/search',
            ],
 }

5857db0cad8c580c01360bd735179f05ce2b2f88
shankaragoudab <140387294+shankaragoudab@users.noreply.github.com>
2024-07-23 16:17:25 +0530

    corrected homefeed end point

--- src/utils/whitelistApis.ts
+++ src/utils/whitelistApis.ts
@@ -3743,7 +3743,7 @@ export const API_LIST = {
                         ROLE.PUBLIC,
                     ],
          },
-        '/proxies/v8/mentoring/v1/mentees/sessions': {
+        '/proxies/v8/mentoring/v1/mentees/homeFeed': {
                     checksNeeded: [CHECK.ROLE],
                     // tslint:disable-next-line: object-literal-sort-keys
                     ROLE_CHECK: [
@@ -4510,7 +4510,7 @@ export const API_LIST = {
             '/proxies/v8/mentoring/v1/mentors/upcomingSessions/:id',
             '/proxies/v8/mentoring/v1/feedback/submit/:id',
             '/proxies/v8/mentoring/v1/cloud-services/getSignedUrl',
-            '/proxies/v8/mentoring/v1/mentees/sessions',
+            '/proxies/v8/mentoring/v1/mentees/homeFeed',
             '/proxies/v8/mentoring/v1/users/list',
             '/proxies/v8/ciosIntegration/v1/loadContentProgressFromExcel',
             '/proxies/v8/cios-enroll/v1/create',

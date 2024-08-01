fb747fddc1e615ad6f9ec0ab45a940ec33a682d1
anilkumar <anilkumar.kammlapalli@tarento.com>
2024-08-01 13:36:45 +0530

    gaven access to mdoLeader for termRetire

--- src/utils/whitelistApis.ts
+++ src/utils/whitelistApis.ts
@@ -4035,6 +4035,7 @@ export const API_LIST = {
                                ROLE_CHECK: [
                                  ROLE.SPV_ADMIN,
                                  ROLE.MDO_ADMIN,
+                                 ROLE.MDO_LEADER,
                               ],
         },
     },
